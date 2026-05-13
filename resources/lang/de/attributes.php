004ef3133abf49b8254ea83aad7ccf31be21fd7d
Misaf <misaf.1990@gmail.com>
2026-05-13 13:07:21 +0330

    feat: add German translations

---
+++ resources/lang/de/attributes.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+return  [
+    'created_at'              => 'Erstellt am',
+    'deleted_at'              => 'Gelöscht am',
+    'description'             => 'Beschreibung',
+    'description_helper_text' => 'Geben Sie eine kurze Beschreibung für diesen FAQ-Inhalt an.',
+    'id'                      => 'ID',
+    'image'                   => 'Bild',
+    'name'                    => 'Name',
+    'slug'                    => 'Slug',
+    'slug_helper_text'        => 'Eine eindeutige Kennung für die URL. Leer lassen, um sie automatisch aus dem Namen zu generieren.',
+    'status'                  => 'Status',
+    'tenant_id'               => 'Mandant',
+    'updated_at'              => 'Aktualisiert am',
+];
---
+++ resources/lang/de/navigation.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+return  [
+    'faq'                     => 'FAQs',
+    'faq_category'            => 'FAQ-Kategorien',
+    'faq_category_management' => 'FAQ-Kategorie-Verwaltung',
+    'faq_management'          => 'FAQ-Verwaltung',
+];
