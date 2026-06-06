fb77ae256b11cccc7f3031fc939a00923e365764
Misaf <misaf.1990@gmail.com>
2026-06-06 13:14:06 +0330

    feat: add blog seed command

---
+++ src/Console/Commands/SeedCommand.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Misaf\VendraFaq\Console\Commands;
+
+use Misaf\VendraFaq\Database\Seeders\DemoContentSeeder;
+use Misaf\VendraFaq\Database\Seeders\PermissionPolicySeeder;
+use Misaf\VendraFaq\FaqPlugin;
+use Misaf\VendraSupport\Console\Commands\BaseSeedCommand;
+
+final class SeedCommand extends BaseSeedCommand
+{
+    protected const string MODULE_NAME = FaqPlugin::ID;
+
+    protected $signature = self::MODULE_NAME . ':seed
+        {tenant : Tenant ID or slug to seed blog data for}
+        {seeders* : Seeder keys to run. Use "all" or one or more of: permissions, contents}';
+
+    protected $description = 'Seed blog module data for a tenant';
+
+    /**
+     * @return array<string, class-string>
+     */
+    protected function seeders(): array
+    {
+        return [
+            'permissions' => PermissionPolicySeeder::class,
+            'contents'    => DemoContentSeeder::class,
+        ];
+    }
+}
