96e10069c46c0bdec44f8c87d994dbb7199ba562
misaf <misaf.1990@gmail.com>
2026-09-17 18:31:16 +0000

    refactor(verification): tidy seeder docblocks and tenant lookups

--- database/seeders/DemoContentSeeder.php
+++ database/seeders/DemoContentSeeder.php
@@ -15,8 +15,6 @@ final class DemoContentSeeder extends BaseDemoContentSeeder
 {
     protected function seedFactories(): void
     {
-        $this->currentTenantOrNull();
-
         FaqCategoryFactory::new()
             ->active()
             ->count(4)
@@ -33,8 +31,6 @@ final class DemoContentSeeder extends BaseDemoContentSeeder
      */
     protected function seedFixtures(array $records): void
     {
-        $this->currentTenantOrNull();
-
         foreach ($records as $record) {
             $this->handleSeedFixtureRecord($this->validatedFixtureRecord($record));
         }
